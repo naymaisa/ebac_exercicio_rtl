93c89c0a129f33b00d89f70e6c2bc32e7d0db3e0
Nayara <nayaramaisa.stn@gmail.com>
2025-05-20 16:17:31 -0300

    teste

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,23 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
-import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe('Inserção de comentários', () => {
+    it('deve permitir inserir dois comentários', () => {
+        render(<Post />);
+
+        const textarea = screen.getByTestId('input-comentario');
+        const botao = screen.getByTestId('botao-enviar');
+
+
+        fireEvent.change(textarea, { target: { value: 'Primeiro comentário' } });
+        fireEvent.click(botao);
+
+
+        fireEvent.change(textarea, { target: { value: 'Segundo comentário' } });
+        fireEvent.click(botao);
+
+
+        expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+        expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
     });
 });
--- src/components/PostComments/index.tsx
+++ src/components/PostComments/index.tsx
@@ -26,8 +26,18 @@ const Post = () => {
                 ))}
             </ul>
             <form onSubmit={handleAddComment} className={styles['post-comments-form']}>
-                <textarea value={tempComment} onChange={e => setTempComment(e.target.value)} required className={styles['post-comments-form-textarea']} />
-                <button type="submit" className={styles['post-comments-form-button']}>
+                <textarea
+                    data-testid="input-comentario"
+                    value={tempComment}
+                    onChange={e => setTempComment(e.target.value)}
+                    required
+                    className={styles['post-comments-form-textarea']}
+                />
+                <button
+                    type="submit"
+                    data-testid="botao-enviar"
+                    className={styles['post-comments-form-button']}
+                >
                     Comentar
                 </button>
             </form>
